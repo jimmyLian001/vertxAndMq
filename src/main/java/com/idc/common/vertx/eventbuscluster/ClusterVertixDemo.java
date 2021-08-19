ef96805d799877f5055f334c289ffe65e3f97c81
lianzidan <zidan.lian@sumscope.com>
2021-08-19 11:15:48 +0800

    添加客户端测试

--- src/main/java/com/idc/common/vertx/eventbuscluster/ClusterVertixDemo.java
+++ src/main/java/com/idc/common/vertx/eventbuscluster/ClusterVertixDemo.java
@@ -1,7 +1,10 @@
 package com.idc.common.vertx.eventbuscluster;
 
+import com.idc.common.po.AppResponse;
 import com.idc.common.po.RpcInvocation;
 import com.idc.common.po.VertxMessageReq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +20,8 @@ import javax.annotation.PostConstruct;
  */
 @Component
 public class ClusterVertixDemo {
+    private static Logger logger = LoggerFactory.getLogger(ClusterVertxClient.class);
+
 
     @Autowired
     private ClusteredVertxServer clusteredVertxServer;
@@ -26,12 +31,12 @@ public class ClusterVertixDemo {
 
     private String eventBusName = "vertx.cluster.replyHello";
 
-    @PostConstruct
+    //    @PostConstruct
     public void initServerCluster() throws Exception {
         clusteredVertxServer.setAndStart(eventBusName);
     }
 
-    //    @PostConstruct
+    @PostConstruct
     public void initCluster() throws Exception {
         clusterVertxClient.setAndStart();
         VertxMessageReq vertxMessageReq = new VertxMessageReq();
@@ -42,7 +47,9 @@ public class ClusterVertixDemo {
         RpcInvocation invocation = new RpcInvocation();
         invocation.setInterfaceName("SayHello");
         invocation.setResource("default");
-        clusterVertxClient.sendMessageToEventBusSyn(eventBusName, vertxMessageReq, 60 * 1000);
+        Thread.sleep(2000);
+        AppResponse result = clusterVertxClient.sendMessageToEventBusSyn(eventBusName, vertxMessageReq, 60 * 1000);
+        logger.info("sendMessageToEventBusSyn result:{}", result);
     }
 
 }
--- src/main/java/com/idc/common/vertx/eventbuscluster/ClusterVertxClient.java
+++ src/main/java/com/idc/common/vertx/eventbuscluster/ClusterVertxClient.java
@@ -1,18 +1,23 @@
 package com.idc.common.vertx.eventbuscluster;
 
 import com.idc.common.po.AppResponse;
-import com.idc.common.po.Result;
 import com.idc.common.po.VertxMessageReq;
 import com.idc.common.util.NetworkUtil;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.spi.cluster.zookeeper.ZookeeperClusterManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author lian zd
@@ -21,34 +26,68 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClusterVertxClient {
     private static Logger logger = LoggerFactory.getLogger(ClusterVertxClient.class);
-    private Vertx clusterVertx = null;
+    private static Vertx clusterVertx;
+    public static final ExecutorService executorService = new ThreadPoolExecutor(8, 16, 10L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
-    public Vertx setAndStart() {
-
+    public void setAndStart() {
         ClusterManager mgr = new ZookeeperClusterManager();
         VertxOptions options = new VertxOptions().setClusterManager(mgr).setClusterHost(NetworkUtil.getInterface());
-        Vertx.clusteredVertx(options, res -> {
+        Vertx.clusteredVertx(options, clusterHandler());
+    }
+
+
+    private Handler<AsyncResult<Vertx>> clusterHandler() {
+        return res -> {
             if (res.succeeded()) {
                 clusterVertx = res.result();
                 logger.info("-------------------start deploy clustered event bus------");
             } else {
                 logger.error("Failed: ", res.cause());
             }
-        });
-        return clusterVertx;
+        };
+
     }
 
-    public void sendMessageToEventBusSyn(String vertxEventBusName, VertxMessageReq vertxMessageReq, long timeOut) {
-        AppResponse appResponse = new AppResponse();
+    public AppResponse sendMessageToEventBusSyn(String vertxEventBusName, VertxMessageReq vertxMessageReq, long timeOut) throws ExecutionException, InterruptedException {
+        CompletableFuture<AppResponse> future = new CompletableFuture<AppResponse>();
         if (clusterVertx != null) {
             vertxMessageReq.setTimeStamp(System.currentTimeMillis());
-            clusterVertx.eventBus().<JsonObject>send("vertx.cluster.replyHello", JsonObject.mapFrom(vertxMessageReq), new DeliveryOptions().setSendTimeout(timeOut), resultBody -> {
-                if (resultBody.failed()) {
-                    logger.error("Fail for the process!");
-                    return;
-                }
-                logger.info("收到服务端响应结果：{}", resultBody.result().body().encode());
+            clusterVertx.eventBus().<JsonObject>send(vertxEventBusName, JsonObject.mapFrom(vertxMessageReq), new DeliveryOptions().setSendTimeout(timeOut), resultBody -> {
+                executorService.submit(new FeatureTask(resultBody, future));
             });
+
+        }
+        return future.get();
+    }
+
+
+    public static class FeatureTask implements Runnable {
+        private AsyncResult<Message<JsonObject>> asyncResult;
+        private CompletableFuture<AppResponse> future;
+
+        public FeatureTask(AsyncResult<Message<JsonObject>> asyncResult, CompletableFuture<AppResponse> future) {
+            this.asyncResult = asyncResult;
+            this.future = future;
+        }
+
+        @Override
+        public void run() {
+            AppResponse appResponse = new AppResponse();
+            try {
+                if (asyncResult.failed()) {
+                    appResponse.setException(asyncResult.cause());
+                    appResponse.setValue("1000");
+                    logger.error("vertx asyncResult error:", asyncResult.cause());
+                } else {
+                    appResponse.setValue(asyncResult.result().body());
+                    logger.error("vertx asyncResult result:{}", appResponse.getValue());
+                }
+                System.out.println(1);
+            } catch (Exception e) {
+                logger.error("FeatureTask run error");
+            }
+            future.complete(appResponse);
+
 
         }
     }
